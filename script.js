a3b28314ff5f0834e45e61e071cdb09797db8bab
Piyush Yadav <85944872+piy-crpto@users.noreply.github.com>
2022-01-22 18:55:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("This is clock.js");
+
 function updateClock()
 {   
     //Get the current date
@@ -9,23 +10,26 @@ function updateClock()
     let currentMinutes = currentTime.getMinutes();
     let currentSeconds = currentTime.getSeconds();
      
+//Choose AM/Pm as per the time of the day
+let timeofDay = (currentHour<12) ? "AM":"PM";
 
      //pad 0 if minutes or second is less than 10 (single digit)
     currentMinutes = (currentMinutes<10 ? "0" : "") + currentMinutes;
     currentSeconds = (currentSeconds<10 ? "0" : "") + currentSeconds;
-   currentHour = (currentHour<10 ? "0" : "") + currentHour;
+   
 
 
    //Convert railway clock to AM/PM clock
   currentHour = (currentHour>12)? currentHour-12:currentHour;
   currentHour = (currentHour==0)? 12:currentHour;
 
-//Choose AM/Pm as per the time of the day
-    let timeofDay = (currentHour<12) ? "AM":"PM";
+  
+  currentHour = (currentHour<10 ? "0" : "") + currentHour;
+
 
 
     //Prepare the time string from hours,minutes and seconds
-    let currentTimeStr = currentHour + ":" + currentMinutes +  ":" + currentSeconds +  " " + timeofDay;
+    let currentTimeStr = currentHour + " : " + currentMinutes +  " : " + currentSeconds +  " " + timeofDay;
 
     //Insert the time string in the DOM
     document.getElementById("clock").innerHTML = currentTimeStr;
